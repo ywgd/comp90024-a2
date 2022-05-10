dafc4c7f926d4966a0c4c6ec6c3b45e99ccd00a1
steamedbeef <87696838+steamedbeef@users.noreply.github.com>
2022-05-10 16:13:35 +0800

    Update new_search.py

--- Crawling Twitter Data/new_search.py
+++ Crawling Twitter Data/new_search.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 consumer_key = "8k7SzAflvtcFmaqJsTiqzylYa"
 consumer_secret = "qPqhLwP77RiuWRgLn8KKPscFLX6PQprPXtrYiaJLxH0Hpe2846"
@@ -10,93 +11,18 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # set wait_on_rate_limit =True; as twitter may block you from querying if it finds you exceeding some limits
-# api = tweepy.API(auth, wait_on_rate_limit=True)
-api = tweepy.API(auth)
-
-# search_words = ["Covid"]
-query = 'Covid'
-
-# tweets = tweepy.Cursor(api.search_tweets, q=query,
-#                        geocode="-37.840935,144.946457,1000km",
-#                        lang="en", result_type="mixed").items(5)
-
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 tweets = tweepy.Cursor(api.search_tweets,
-                       q="#covid OR covid-19 geocode:-37.840935,144.946457,2000km",
-                       lang="en").items(10)
+                       q="covid OR coronavirus geocode:-37.840935,144.946457,40km -filter:retweets AND -filter:quote",
+                       lang="en").items(10000)
 
 # the geocode is for melbourne; format for geocode="latitude,longitude,radius"
-# radius should be in km
+# radius should be in km/mi
+
 
 for tweet in tweets:
-    print("created_at: {}\nuser: {}\ntweet text: {}\ngeo_location: {}".
-          format(tweet.created_at, tweet.user.screen_name, tweet.text, tweet.user._json["geo_enabled"]))
-    print("\n")
-
-# tweet.user.location will give you the general location of the user and not the particular location for the tweet
-# itself, as it turns out, most of the users do not share the exact location of the tweet
-
-
-'''
-import tweepy as tw
-
-bearer_token = "AAAAAAAAAAAAAAAAAAAAACAQbwEAAAAAMMc5leYe2RxXTUO4xNKAhSp9DuI%3D3AI6uRezA6X1teJIzd0WYMh53nIjyE421lYPj4o9RPJNmEeRgY"
-client = tw.Client(bearer_token)
-
-query = 'covid -is:retweet'
-end_time = '2022-05-01T15:46:00Z'
-
-# print("-----------------------------------------------")
-response = client.search_recent_tweets(query = query, tweet_fields=['context_annotations', 'created_at', 'geo'], place_fields=['place_type', 'geo'], expansions='geo.place_id', end_time = end_time)
-print("-----------------------------------------------")
-# places = {p["id"]: p for p in response.includes['places']}
-# print(response.meta)
-# print("-----------------------------------------------")
-
-tweets = response.data
-for tweet in tweets:
-    print(tweet.id)
-    print(tweet.text)
-    # print(tweet.location)
-    # if tweet.geo != None:
-        # print(tweet.text.encode('utf-8', errors='ignore'))
-
-    if places[tweet.geo['place_id']]:
-        place = places[tweet.geo['place_id']]
-        print(place.full_name)
-'''
-
-'''
-import sys
-import os
-import re
-import tweepy
-import pandas as pd
-from tweepy import OAuthHandler
-# from textblob import TextBlob
-
-consumer_key = "8k7SzAflvtcFmaqJsTiqzylYa"
-consumer_secret = "qPqhLwP77RiuWRgLn8KKPscFLX6PQprPXtrYiaJLxH0Hpe2846"
-# access_token = "1517355964617297920-mOWbcyxo7Zr9SXsRN8nXIn9zrm2NpT"
-# access_token_secret = "zvJ7znhMV4ZmVG6fKl3t4w7e4McNUU7x8cCTGNympdwqD"
-
-#obtaining tweets within a certain distance from a specific location, and converting important attributes into a dataframe
-auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
-api = tweepy.API(auth, wait_on_rate_limit = True)
-if not api:
-    print("Can't Authenticate")
-    sys.exit(-1)
-
-tweet_lst = []
-geoc = "-37.840935, 144.946457, lmi"
-query = ["covid"]
-for tweet in tweepy.Cursor(api.search_tweets, geocode = geoc, q = query).items(10):
-    tweetDate = tweet.created_at.date()
-    if tweet.coordinates != None:
-        tweet_lst.append([tweetDate,tweet.id,tweet.coordinates["coordinates"][0],
-                tweet.coordinates["coordinates"][1],
-                tweet.user.screen_name,
-                tweet.user.name, tweet.text,
-                tweet.user._json["geo_enabled"]])
-tweet_df = pd.DataFrame(tweet_lst, columns=['tweet_dt', 'id', 'lat','long','username', 'name', 'tweet','geo'])
-'''
+    with open('recent_tweet_comma.json', 'a', encoding='utf-8') as tf:
+        tf.write('\n')
+        json.dump(tweet._json, tf)
+        tf.write(',')

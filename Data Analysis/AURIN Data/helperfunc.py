ccf92b90da0d8e606461be50a3fcd1d62dfff07c
Yuanwei Mao <3511227125@qq.com>
2022-05-17 01:13:19 +0800

    rename directory

--- Data Analysis/AURIN Data/helperfunc.py
+++
@@ -1,6 +0,0 @@
-def Replace_None(dictionary, replacement):
-    for k, v in dictionary.items():
-        if isinstance(v, dict):
-             Replace_None(v)
-        elif v is None:
-             dictionary[k] = replacement
